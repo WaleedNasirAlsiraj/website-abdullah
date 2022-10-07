4f97b0099944e7da886927d19d1dcef54191ec6d
WaleedNasirAlsiraj <107930810+WaleedNasirAlsiraj@users.noreply.github.com>
2022-10-07 16:33:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+ScrollReveal({ distance: '80px' });
+ScrollReveal().reveal('.one' , {
+    duration : 1000 ,
+    orgin: 'bottom'
+});
+ScrollReveal().reveal('.two' , {
+    duration : 1000 ,
+    delay :200,
+    orgin: 'bottom'
+});
+ScrollReveal().reveal('.three' , {
+    duration : 1000 ,
+    delay :400,
+    orgin: 'bottom'
+});
+ScrollReveal().reveal('.four' , {
+    duration : 1000 ,
+    delay :600,
+    orgin: 'bottom'
+});
+ScrollReveal().reveal('.reveal' , {
+    duration : 500 ,
+    delay :500,
+    orgin: 'bottom'
+});
